0879d96a92a3b495fc56276a6f25b2bbdb39ecfa
jamiddd <jamiddeka1@gmail.com>
2019-12-23 22:04:07 +0530

    Added laravel collective

--- resources/views/pages/admission.blade.php
+++ resources/views/pages/admission.blade.php
@@ -13,37 +13,28 @@
                     <p>This is the application form for class XI. Students from class X can apply here.</p>
                     <div class="row" style="margin-top:100px;">
                         <div class="col s12 m12 l12">
-                            <form action="">
-                                <div class="row">
-                                    <div class="input-field col s12 m7 l7">
-                                        <select style="border: 1px solid #f2f2f2;">
-                                            <option value="" disabled selected>Choose your option</option>
-                                            <option value="1" style="font-family:Arial, Helvetica, sans-serif;">Science
-                                            </option>
-                                            <option value="2" style="font-family:Arial, Helvetica, sans-serif;">Arts
-                                            </option>
-                                            <option value="3" style="font-family:Arial, Helvetica, sans-serif;">Commerce
-                                            </option>
-                                        </select>
-                                        <label class="black-text" style="margin-bottom:30px !important;">Select the
-                                            course you want
-                                            to apply for</label>
-                                    </div>
-                                    <div class="input-field col s12 m7 l7">
-                                        <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                                        <label for="first_name">First Name</label>
-                                    </div>
-                                    <div class="input-field col s12 m7 l7">
-                                        <input placeholder="Placeholder" id="last_name" type="text" class="validate">
-                                        <label for="last_name">Last Name</label>
-                                    </div>
+                           {!! Form::open(['url' => '/', 'mehtod' => 'post', 'files' => 'true'])!!}
+                                {{Form::token()}}
+                                {{Form::label('username', 'Username', ['class' => 'username'])}}
+                                {{Form::text('username')}}
+                                {{Form::select('size', ['L' => 'Large', 'S' => 'Small'], null, ['placeholder' => 'Pick a size...'])}}
+                                {{Form::submit('Submit', ['class' => 'btn blue'])}}
+                           {{-- Attaching the CSRF token in route mehtod --}}
+                           {{--
+                                Route::post('profile',
+                                    [
+                                        'before' => 'csrf',
+                                        function()
+                                        {
+                                            //
+                                        }
+                                    ]
+                                );
+                            --}}
+                           {{-- Can pass controller mehtod as URL also. such as Controller@method --}}
 
-                                    {{-- <div class="input-field col s6">
-                                        <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                                        <label for="first_name">First Name</label>
-                                    </div> --}}
-                                </div>
-                            </form>
+
+                           {!! Form::close() !!}
                         </div>
                     </div>
                 </div>
